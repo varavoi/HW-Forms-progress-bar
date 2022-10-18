f4040454d4abddc4ee76d9156ecff5912950f3ad
Vardan <Vardan@COMPUTER>
2022-10-18 23:23:22 +0300

    Добавьте файлы проекта.

---
+++ HW Forms progress bar/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_Forms_progress_bar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (progressBar1.Value < progressBar1.Maximum)
+            {
+                progressBar1.Value += progressBar1.Step;
+                 Thread.Sleep(100);
+            }
+            
+        }
+    }
+}
